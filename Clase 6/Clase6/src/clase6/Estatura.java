0b133cbaf3e8ae45d54e050220953259c66ca5d3
Carlos Castillo <carloscastillo090916@gmail.com>
2018-05-10 16:14:05 -0500

    Actualizando mejora

--- Clase 6/Clase6/src/clase6/Estatura.java
+++ Clase 6/Clase6/src/clase6/Estatura.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
 
  */
+/*
+    Programa que permite ingresar las estaturas de varios estudiantes y presentar en pantalla todas las estaturas ingresadas y cacular el 
+    promedio de estatura
+*/
 
 package clase6;
 
@@ -23,21 +27,24 @@ public class Estatura {
         double estatura_minima = 1.20;
         boolean verificar = true;
         int contador = 0;
-        cadena_final = String.format("%s\n", "Reporte de estaturas");
+        
+        cadena_final = String.format("%s\n", "Reporte de estaturas");//Valor inicial que toma la variable cadena
         System.out.printf("INGRESO DE ESTATURAS DE LOS ESTUDIANTES\n");
        
         while(verificar){
             System.out.printf("\n\tIngrese la estatura numero %d: ", contador);      
             estatura = entrada.nextDouble();
-            if (estatura < estatura_minima){
+            if (estatura < estatura_minima){//Condicion que compara si la estatura ingresadaa es mayor a la estatura media.
                 estatura = estatura_minima;    
            }
             suma_estatura = suma_estatura + estatura;
-             cadena_final = String.format("%s %.2f\n",cadena_final,estatura);
-            entrada.nextLine();
+            cadena_final = String.format("%s %.2f\n",cadena_final,estatura);//Concatenacion de cadenas
+            entrada.nextLine();//Limpiador de Scanner
+           
             contador = contador + 1;
-            System.out.printf("\nDesea ingresar mas estaturas?\n");
-            System.out.printf("Ingrese no(SALIR): ");
+           
+            System.out.printf("\n\t1. Ingrese 'si' para-->Nueva Estatura\n\t2. Ingrese 'no' para--> Presentar Promedio\n");
+            System.out.printf("Escriba su respuesta: ");//Menu que permite verificar si el usuario desea ingresar  otra edad o presentar el promedio.
             String respuesta = entrada.nextLine();
             
             if (respuesta.equals("no")){
@@ -47,7 +54,7 @@ public class Estatura {
         }
         promedio_estatura = suma_estatura / contador;
         System.out.printf("\n%s",cadena_final);
-        System.out.printf("Promedio estaturas: %.2f\n", promedio_estatura);
-        System.out.printf("Reporte generado por Dep. Deportes\n");
+        System.out.printf("\nPromedio estaturas: %.2f\n", promedio_estatura);
+        System.out.printf("\nReporte generado por Dep. Deportes\n\n".toUpperCase());
     }
 }
